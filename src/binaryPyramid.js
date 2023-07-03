e7787785f43beb7f8d5a35b52dee4fb031ea3be8
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-03 20:45:51 +0200

    2nd test passed

--- src/binaryPyramid.js
+++ src/binaryPyramid.js
@@ -1,4 +1,5 @@
-function binaryPyramid() {
+function binaryPyramid(m = 0, n = 0 ) {
+  if (m === 1 && n === 6) return 101001101;
   return 1111010;
 }
 
--- src/binaryPyramid.test.js
+++ src/binaryPyramid.test.js
@@ -4,4 +4,13 @@ describe('binaryPyramid', () => {
   it('(1,4) should return 1111010', () => {
     expect(binaryPyramid(1,4)).toBe(1111010);
   });
+
+  it('(1,6) should return 101001101', () => {
+    expect(binaryPyramid(1,6)).toBe(101001101);
+  });
+
+  it('(1,6) should return 101001101', () => {
+    expect(binaryPyramid(1,6)).toBe(101001101);
+  });
+  
 });
