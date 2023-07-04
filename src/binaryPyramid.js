442a8e1fe10ff154c5c116b42b1be07386404d3d
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-04 08:55:58 +0200

    Final and submited solution

--- src/binaryPyramid.js
+++ src/binaryPyramid.js
@@ -1,15 +1,10 @@
 function binaryPyramid(m = 0, n = 0 ) {
 
-
-
   let result = 0;
 
   for ( let i = m; i <= n; i++){
     const binaryNumber = parseInt(i.toString(2));
     result += binaryNumber;
-    console.log('I: ---> ',i)
-    console.log('binaryNumber: ---> ',binaryNumber)
-
   }
 
   return result.toString(2);
